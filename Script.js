c32b16afa52c4104709749f1ed0ce981928e3db5
Suraj Pandey <90378868+sp978077@users.noreply.github.com>
2022-05-01 18:56:52 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,13 @@
+function myFunction (){
 
+var img = document.createElement("img");
+
+    img.src = "ant.png";
+
+var div = document.getElementById("1");
+
+    div.appendChild(img);
+
+}
+
+document.getElementById("button").addEventListener("click", myFunction );
